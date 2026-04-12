1f75db3782c5748d45d1ee1b6795152b3394ddf4
A5Beef <edvintran@gmail.com>
2026-04-12 22:09:15 +0300

    gitignore changes, new dependency, linting and new invoke command gitignore added pygame wheel file poetry added autopep8 and a new invoke command for this Linted project files

--- src/board.py
+++ src/board.py
@@ -4,20 +4,21 @@ from .tetrominoes import Tetromino, TetrominoType
 BOARD_WIDTH = 10
 BOARD_HEIGHT = 20
 
+
 class Board:
-    def __init__(self): #alustava, random palikat tyhjä kenttä
+    def __init__(self):  # alustava, random palikat tyhjä kenttä
         self.grid = [[None]*10 for _ in range(20)]
         self.currentblock = Tetromino(random.choice(list(TetrominoType)))
         self.nextblock = Tetromino(random.choice(list(TetrominoType)))
         self.score = 0
         self.gameover = False
 
-    def _is_valid_position(self, piece: Tetromino): #tarkistaa tyhjä tai reunat
+    def _is_valid_position(self, piece: Tetromino):  # tarkistaa tyhjä tai reunat
         for x, y in piece.get_blocks():
-            if x < 0 or x >= 10: #tarkistaa vasen ja oikean reunan
+            if x < 0 or x >= 10:  # tarkistaa vasen ja oikean reunan
                 return False
-            if y >= 20: #tarkistaa alareunan
+            if y >= 20:  # tarkistaa alareunan
                 return False
-            if self.grid[y][x] is not None: #tarkistaa onko ruutu tyhjä
+            if self.grid[y][x] is not None:  # tarkistaa onko ruutu tyhjä
                 return False
-        return True #tarkistetty, ok
+        return True  # tarkistetty, ok
--- src/index.py
+++ src/index.py
@@ -1,3 +1,3 @@
-import os # pylint: disable=unused-import
-import pygame # pylint: disable=unused-import
-#work in progress
+import os  # pylint: disable=unused-import
+import pygame  # pylint: disable=unused-import
+# work in progress
